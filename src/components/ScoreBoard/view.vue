b7f3b79cd5abfcf0e1a6375144307f5795a4010e
AokiMasataka <4okimasataka@gmail.com>
2024-12-26 18:50:20 +0900

    refcat components

---
+++ src/components/ScoreBoard/view.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="mx-8 my-2">
+        <p>Win: {{ results.win }}</p>
+        <p>Lose: {{ results.lose }}</p>
+        <p>Draw: {{ results.draw }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    results: {
+        win: number,
+        lose: number,
+        draw: number
+    }
+}>();
+
+</script>
