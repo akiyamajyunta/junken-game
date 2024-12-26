dc07d93aefe991af7aceec1c2de7beaa47b0a029
AokiMasataka <4okimasataka@gmail.com>
2024-12-26 18:52:43 +0900

    feat: Chara card

---
+++ src/components/CharactarCard/view.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card
+        class="item"
+        width="100"
+        height="150"
+    >
+        <v-img class="mt-4" :src="props.image" cover/>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{image?: string}>();
+</script>
+
+<style lang="css" scoped>
+.item {
+    border-width: 3px;
+    border-style: solid;
+    border-color: rgb(255 255 255);
+    border-radius: 8px;
+}
+</style>
